5743e2c7386e7a43bf1f7c4027db997e384d912f
Egor Shalaev <barnagid@gmail.com>
2021-11-18 22:26:11 +0100

    fingeteq

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,13 +1,4 @@
 /// <reference types="Cypress" />
 
-it.only('using get find eq', () => {
-    cy.visit('https://next.privat24.ua/deposit/open')
-    cy.get('tbody').find('td').find('div').find('button').eq('0')
-});
 
-//проверить цепочку селекторов
-// it.only('using get find eq', () => {
-//     cy.viewport(1800, 700)
-//     cy.visit('https://docs.cypress.io/api/commands/eq#Syntax')
-//     cy.get('aside').find('div').find('ol').find('li').find('a')
-// });
+// find get eq
